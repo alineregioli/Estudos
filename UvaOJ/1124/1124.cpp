bf9790cb4f7643ba158647ab1da5529059ebdc79
alineregioli <alineregioli@hotmail.com>
2017-01-20 22:45:45 -0200

    1124

---
+++ UvaOJ/1124/1124.cpp
@@ -0,0 +1,15 @@
+//Uva 1124 Celebrity jeopardy
+//alineregioli - 20 Janeiro 2017
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+char linha[300];
+
+int main(){
+	while(~scanf(" %[^\n]",linha)){
+		printf("%s\n",linha ); 
+	}
+	return 0;
+}	
